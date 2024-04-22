98cddbae0cde5d6e8257e5946d753312cab32430
mouli-daran <moulidaran684@gmail.com>
2024-04-22 09:54:27 +0530

    updated logout page

---
+++ src/Components/auth/Logout.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import styles from "./logout.module.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Logout = ({ parent, userDetails }) => {
+  const navigate = useNavigate();
+  const backendurl = `http://localhost:4000/api/v1/logout`;
+
+  const handleLogout = () => {
+    const loggedOut = axios.get(backendurl);
+    console.log(loggedOut);
+    toast.info("Logged out!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    if (parent === "home") {
+      Cookies.remove("token");
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      window.location.reload();
+      navigate("/");
+    } else {
+      Cookies.remove("token");
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+    }
+  };
+
+  return (
+    <div className={styles.logoutContainer}>
+      {/* <p className={styles.logUsername}>{userDetails.user.username}</p> */}
+      <button className={styles.logoutBtn} onClick={handleLogout}>
+        Logout
+      </button>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Logout;
