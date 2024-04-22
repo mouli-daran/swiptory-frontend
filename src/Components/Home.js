bea71ce2b22d67aded770ea534e1ee7e3d1d8630
mouli-daran <moulidaran684@gmail.com>
2024-04-22 09:25:08 +0530

    created homepage and imported images

---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import "./home.css";
+import styles from "./bookmarkpage.module.css";
+import Bookmarkicon from "../Image/bookmark-icon.png";
+import Profilepic from "../Image/profilepic.svg";
+import Hamburgericon from "../Image/Ham.svg";
+import all from "../Image/image 466.png";
+import food from "../Image/fillimg3.jpg";
+import travel from "../Image/fillimg2.jpg";
+import health from "../Image/medicine.jpg";
+import movies from "../Image/movie.jpg";
+import education from "../Image/fillimg4.png";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
