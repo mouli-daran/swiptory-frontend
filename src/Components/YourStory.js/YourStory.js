7a6fc1ceb1bc749d2096b63c9095d085990fd5ee
mouli-daran <moulidaran684@gmail.com>
2024-04-27 21:34:01 +0530

    added stories page and view

---
+++ src/Components/YourStory.js/YourStory.js
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import filters from "../Filters/filters.module.css";
+import EditStories from "../storiesmodal/EditStoriesModal";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+const YourStroy = ({ userId, isLoggedIn }) => {
+  const navigate = useNavigate();
+  const initialVisibleIndiaImages = 4;
+  const [stories, setStories] = useState();
+  const [visibleIndiaImages, setVisibleIndiaImages] = useState(
+    initialVisibleIndiaImages
+  );
+  const [openEditStoriesModal, setOpenEditStoriesModal] = useState(false);
+  const [storyId, setStoryId] = useState(null);
+  const [statestories, setStateStories] = useState([]);
+
+  const backendUrl = `http://localhost:4000/api/v1/stories/userstories/${userId}`;
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const result = await axios.get(backendUrl);
+        const data = result.data;
+        if (data.success) {
+          setStories(data.userStories);
+          console.log(stories);
+        } else {
+          toast.error("No more stories are avaible");
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetch();
+  }, [isLoggedIn, userId]);
+
+  const handleSeeMoreIndiaClick = () => {
+    setVisibleIndiaImages(visibleIndiaImages + 4);
+  };
+
+  const HandleEditModal = (e) => {
+    e.stopPropagation();
+    setStoryId(e.target.getAttribute("id"));
+    setOpenEditStoriesModal(true);
+  };
+
+  const individualStoryPage = (e) => {
+    const storyId = e.target.getAttribute("id");
+    console.log(e.target.getAttribute("id"));
+    navigate(`/individualstory/${storyId}`);
+  };
+
+  return (
+    <>
+      <h1 className={filters.yourStoryHeading}>Your stories </h1>
+
+      {stories && stories.length === 0 ? (
+        <h1 className={filters.NoMoreStories}>No stories are available</h1>
+      ) : (
+        <>
+          {stories &&
+            stories.length > 0 &&
+            stories.slice(0, visibleIndiaImages).map((story) => {
+              return (
+                <div
+                  className={`${filters.storycontainer} ${filters.background} ${filters.container}`}
+                  style={{
+                    backgroundImage: `url(${
+                      story.slides &&
+                      story.slides[0] &&
+                      story.slides[0].image &&
+                      story.slides[0].image.url
+                    })`,
+                  }}
+                  id={story._id}
+                  key={story._id}
+                  onClick={individualStoryPage}
+                >
+                  <div
+                    className={filters.wrappered}
+                    id={story._id}
+                    onClick={individualStoryPage}
+                  >
+                    <h3
+                      className={filters.heading}
+                      id={story._id}
+                      onClick={individualStoryPage}
+                    >
+                      {story.slides && story.slides[0].heading}
+                    </h3>
+                    <p
+                      className={filters.decsription}
+                      id={story._id}
+                      onClick={individualStoryPage}
+                    >
+                      {story.slides && story.slides[0].description}
+                    </p>
+                  </div>
+
+                  <button
+                    className={filters.editBtn}
+                    onClick={HandleEditModal}
+                    id={story._id}
+                  >
+                    <FontAwesomeIcon icon={faPen} /> Edit
+                  </button>
+                </div>
+              );
+            })}
+          {openEditStoriesModal ? (
+            <EditStories
+              setOpenEditStoriesModal={setOpenEditStoriesModal}
+              storyId={storyId}
+              openEditStoriesModal={openEditStoriesModal}
+            />
+          ) : null}
+          {stories && stories.length > visibleIndiaImages && (
+            <button
+              className={filters.seeMoreBtn}
+              onClick={handleSeeMoreIndiaClick}
+            >
+              See more
+            </button>
+          )}
+        </>
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
+      <ToastContainer />
+    </>
+  );
+};
+
+export default YourStroy;
---
+++ src/Components/individualstories/IndividualStory.js
@@ -0,0 +1,355 @@
+import { useState, useEffect } from "react";
+import SignIn from "../auth/Signin";
+import styles from "./individualStory.module.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useParams, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faBookmark,
+  faChevronLeft,
+  faChevronRight,
+  faPaperPlane,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
+
+const IndividualStory = ({ handleSigninClick }) => {
+  const [story, setStory] = useState();
+  const [currentslide, setCurrentSlide] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginComponent, setLoginComponent] = useState(false);
+  const [userId, setUserId] = useState();
+
+  const { id } = useParams();
+  const storyId = id;
+
+  const navigate = useNavigate();
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const backendUrl = `http://localhost:4000/api/v1/stories/${storyId}`;
+  const backendUrlEdit = `http://localhost:4000/api/v1/stories/editstory/${storyId}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(backendUrl);
+        if (result.data.success) {
+          setStory(result.data.story);
+          console.log(story);
+        } else {
+          toast.error(result.data.message);
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+
+    let userId = localStorage.getItem("userId");
+    if (userId) {
+      setIsLoggedIn(true);
+      userId = Number(userId);
+      setUserId(userId);
+    }
+  }, []);
+
+  const previousSlide = () => {
+    setCurrentSlide((currentslide) => {
+      if (currentslide > 0) {
+        return currentslide - 1;
+      }
+      console.log(currentslide);
+      return currentslide;
+    });
+  };
+
+  // const nextSlide = () => {
+  //   setCurrentSlide((currentslide) => {
+  //     if (story && story.slides && currentslide < story.slides.length - 1) {
+  //       return currentslide + 1;
+  //     } else {
+  //       toast.error("No more slides available");
+  //     }
+  //     console.log(currentslide);
+  //     return currentslide;
+  //   });
+  // };
+
+  const nextSlide = () => {
+    setCurrentSlide((currentslide) => {
+      if (story && story.slides && currentslide < story.slides.length - 1) {
+        return currentslide + 1;
+      } else {
+        toast.error("No more slides available");
+      }
+      console.log(currentslide);
+      return currentslide;
+    });
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (story && story.slides && currentslide < story.slides.length - 1) {
+        nextSlide();
+      } else {
+        clearInterval(timer);
+      }
+    }, 10000);
+
+    return () => clearInterval(timer);
+  }, [story, currentslide]);
+
+  const handleBookmark = async () => {
+    if (!isLoggedIn) {
+      setLoginComponent(true);
+      toast.error("Login Plese !!");
+    } else {
+      const previousStory = story;
+      const updatedStory = {
+        ...previousStory,
+        bookmark: story.bookmark ? false : true,
+      };
+
+      try {
+        const result = await axios.put(backendUrlEdit, updatedStory);
+        if (result.data.success) {
+          console.log(updatedStory, "updated at api");
+          setStory(updatedStory);
+        }
+      } catch (error) {
+        toast.error("error");
+      }
+    }
+  };
+
+  // const handleBookmark = async () => {
+  //   if (!isLoggedIn) {
+  //     setLoginComponent(true);
+  //     toast.error("Login Plese !!");
+  //   } else {
+  //     const previousStory = story;
+  //     console.log(previousStory);
+  //     let updatedStory;
+  //     if (story.bookmark) {
+  //       updatedStory = {
+  //         ...previousStory,
+  //         bookmark: false,
+  //         userId: previousStory.userId.filter((id) => id !== userId),
+  //       };
+  //     } else {
+  //       updatedStory = {
+  //         ...previousStory,
+  //         bookmark: true,
+  //         userId: previousStory.userId.includes(userId)
+  //           ? previousStory.userId
+  //           : [...previousStory.userId, userId],
+  //       };
+  //     }
+
+  //     console.log(updatedStory);
+
+  //     try {
+  //       const result = await axios.put(backendUrlEdit, updatedStory);
+  //       if (result.data.success) {
+  //         console.log(updatedStory, "updated at api");
+  //         setStory(updatedStory);
+  //       }
+  //     } catch (error) {
+  //       toast.error("error");
+  //     }
+  //   }
+  // };
+
+  const closeModal = () => {
+    navigate("/");
+  };
+
+  const handleLike = async () => {
+    if (!isLoggedIn) {
+      toast.error("Login Plese !!");
+    } else {
+      setStory((previousStory) => {
+        const updatedSlides = previousStory.slides.map((slide, index) => {
+          if (index === currentslide) {
+            if (slide.liked) {
+              toast.error("Unliked!!");
+              return { ...slide, like: slide.like - 1, liked: false };
+            } else {
+              toast.success("🦄 Wow!", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+              return { ...slide, like: slide.like + 1, liked: true };
+            }
+          } else {
+            return slide;
+          }
+        });
+
+        const updatedStory = { ...previousStory, slides: updatedSlides };
+        try {
+          const result = axios.put(backendUrlEdit, updatedStory);
+          if (result.data.success) {
+            console.log(updatedStory);
+            setStory(updatedStory);
+          } else {
+            toast.error("error");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+
+        return updatedStory;
+      });
+    }
+  };
+
+  return (
+    <div className={styles.background}>
+      {story && story.slides && story.slides[currentslide] && (
+        <div
+          className={`${styles.container} ${styles.backgroundImage}`}
+          style={{
+            backgroundImage: `url(${story.slides[currentslide].image.url})`,
+          }}
+        >
+          {/* <div className={styles.slideContainer}>
+            {story.slides.map((slide, index) =>
+              index < currentslide ? (
+                <span
+                  key={index}
+                  className={`${styles.slide} ${styles.dark}`}
+                ></span>
+              ) : (
+                <span key={index} className={styles.slide}></span>
+              )
+            )}
+          </div> */}
+
+          <div className={styles.slideContainer}>
+            {story.slides.map((slide, index) =>
+              index < currentslide ? (
+                <span
+                  key={index}
+                  className={`${styles.slide} ${styles.dark}`}
+                ></span>
+              ) : index === currentslide ? (
+                <span
+                  key={index}
+                  className={`${styles.slide} ${styles.dark} ${styles.animated}`}
+                ></span>
+              ) : (
+                <span key={index} className={styles.slide}></span>
+              )
+            )}
+          </div>
+
+          <div className={styles.ButtonContainer}>
+            <button className={styles.slideCloseBtn} onClick={closeModal}>
+              <FontAwesomeIcon icon={faX} />
+            </button>
+
+            <button
+              className={styles.slideCloseBtn}
+              onClick={() => {
+                toast.success("🦄 Link Copied !", {
+                  position: "top-center",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+              }}
+            >
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </div>
+
+          <div>
+            <button className={styles.leftSlide} onClick={previousSlide}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <button className={styles.rightSlide} onClick={nextSlide}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          </div>
+
+          {/* Heading and description */}
+          <div className={styles.detailsContainer}>
+            <h1 className={styles.align}>
+              {story.slides[currentslide].heading}
+            </h1>
+            <p className={styles.descriptionAlign}>
+              {story.slides[currentslide].description}
+            </p>
+
+            {/* Bookmark and heart button */}
+            <div className={styles.ButtonContainerBookmark}>
+              {story.bookmark ? (
+                <button
+                  className={`${styles.slideBookmarkBtn} ${styles.bookmarkedBtn}`}
+                  onClick={handleBookmark}
+                >
+                  <FontAwesomeIcon icon={faBookmark} />
+                </button>
+              ) : (
+                <button
+                  className={styles.slideBookmarkBtn}
+                  onClick={handleBookmark}
+                >
+                  <FontAwesomeIcon icon={faBookmark} />
+                </button>
+              )}
+
+              <button
+                className={`${styles.slideLikeBtn} ${
+                  story.slides[currentslide].liked ? styles.liked : ""
+                }`}
+                onClick={handleLike}
+              >
+                <FontAwesomeIcon icon={faHeart} />
+                <span className={styles.likeNumber}>
+                  {story.slides[currentslide].like}
+                </span>
+              </button>
+            </div>
+          </div>
+          {loginComponent && (
+            <SignIn
+              setLoginComponent={setLoginComponent}
+              parent="individualStory"
+            />
+          )}
+        </div>
+      )}
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
+
+export default IndividualStory;
